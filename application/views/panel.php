91b8277f27af964d8b9d92cbd75e7123cada1908
Connor MacD <connormacd@github.com>
2015-01-26 18:04:34 -0400

    added back-end panel

---
+++ application/views/panel.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Connor
+ * Date: 1/26/2015
+ * Time: 5:14 PM
+ */ ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Back-end Panel</title>
+        <?php $data = $this->session->userdata('logged_in'); ?>
+    </head>
+
+    <body>
+        <header>
+            <h1>Back-end Panel</h1>
+        </header>
+
+
+        <!--Editor roles? Link those pages here-->
+        <?php if(in_array(2, $data['roles'])):?>
+            <h2>Editor Settings</h2>
+            <ul>
+                <li><?php echo anchor("panel/spreadsheets/", "Edit Spreadsheets")?></li>
+            </ul>
+        <?php endif ?>
+    </body>
+</html>
