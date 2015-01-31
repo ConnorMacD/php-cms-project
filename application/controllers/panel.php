71b1ca4de4467ea9ea966eb18b26e09e7b662b60
hashedtox <connormacd@github.com>
2015-01-31 19:49:28 -0400

    added more docs for adding more panel elements

--- application/controllers/panel.php
+++ application/controllers/panel.php
@@ -21,23 +21,51 @@ class Panel extends CI_Controller {
         }
     }
 
+    /*
+     * TODO: Add authentication to all CRUD elements when finished
+     * TODO: Add more style to the back-end panel
+     * */
+
     function stylesheets() {
+
+        //Make the object. use this object type
         $crud = new ajax_grocery_CRUD();
 
+        //set table. Then set columns. Can be done in one line or 2.
         $crud->set_table('stylesheet')
              ->columns('name','content','description','activeState',
                  'createsByIndicator', 'creationDate','lastModifyBy',
                  'modifyDate');
+        //adds the relations. Format: set_relation('columnThatShouldBeRelated', 'tableThatIsRelated', 'columnThatHasRelatedData');
+        //So in this case, the createsByIndicator relates to the 'user' table, and will show 'username' on a related field.
+        $crud->set_relation('createsByIndicator','users', 'username');
+        //Controls what fields are shown on the 'add' stylesheets page.
         $crud->add_fields('name','content','description','activeState','createsByIndicator','creationDate');
+
         //$session_data = $this->session->userdata('logged_in');
-        //$crud->field_set_defaults('createsByIndicator','readonly', '1');
+        //will be using the stored session's id to automatically assign the ID.
+        //time will be saved as TEXT, because DATETIME has a specific format.
+        //this may not fully work until you set all DATETIME fields to TEXT.
+
+        //field_type sets a 'default' value on a specific type.
+        //Here, we use it on 'createsByIndicator'. 'hidden' causes it to not be shown.
+        //We hide it so the user doesn't have to worry about it.
+        //The third parameter will be what that field is set to.
+        //Because no logins are set currently when accessing this page, the default is userid:1.
+        //It will be changed to grab the current session's later when its more set up.
+
         $crud->field_type('createsByIndicator','hidden',1);
         $crud->field_type('creationDate','hidden','date');
-        //$crud->field_set_defaults('creationDate','readonly',time('mm/dd/yy h:i:s'));
-        //$crud->change_field_type('createsByIndicator', 'disabled');
+
+
+        //this renders the crud view. We put it in $output as whats returned is an array.
         $output = $crud->render();
 
+        //$output is sent along as data with the view.
         $this->load->view('crud_view',$output);
+
+
+        //and that's it!
 
     }
 
